009daa1d33f614554dd2797755ba51436061af82
yanvitech <precieuxvivien@gmail.com>
2025-01-27 13:58:32 +0100

    La fontion de calcule de l'air total

--- paveMagique.dart
+++ paveMagique.dart
@@ -1,9 +1,16 @@
 void main() {
   double volume = calculVolumePave(32, 43, 5);
   print(volume);
+  double aire = calculSurfaceTotalePave(3, 6, 23);
+  print(aire);
 }
 
 double calculVolumePave(double largeur, double longeur, double hauteur) {
   double volume = largeur * longeur * hauteur;
   return volume;
+}
+double calculSurfaceTotalePave(double largeur, double longeur, double hauteur){
+  double air =
+      2 * ((largeur * longeur) + (longeur * hauteur) + (largeur * hauteur));
+  return air;
 }
