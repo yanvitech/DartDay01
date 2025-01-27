5200375d706640219336c3d82ce680631c97dd6f
yanvitech <precieuxvivien@gmail.com>
2025-01-27 19:08:12 +0100

    la fontion lancer de dé qui affiche les resultats de plusieurs lancer

--- folidesde.dart
+++ folidesde.dart
@@ -2,10 +2,25 @@ import 'dart:math';
 void main() {
    int nombreAlea = lancerDe();
   print(nombreAlea);
+   int nombreDeLancer = lancerDeMultiple(5);
+  print(nombreDeLancer);
+
 }
 
 int lancerDe(){
   var random = Random();
   int randomint = random.nextInt(7); 
   return randomint;
+}
+
+  
+
+int lancerDeMultiple(int nombreDeLancers){
+  
+  for(int i = 0; i < nombreDeLancers ; i++){
+     var random = Random();
+    int randomint = random.nextInt(7);
+    print(randomint);
+  }
+  return 0;
 }
